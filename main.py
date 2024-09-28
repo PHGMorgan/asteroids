6ade8839494fb35dd24e56eea6cecd28d69f08e5
PHGMorgan <morgan.hutchens777@gmail.com>
2024-09-28 15:38:43 -0700

    Added circleshape.py, changed main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
 
-black_color = (0, 0, 0)
+
 
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +20,8 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":

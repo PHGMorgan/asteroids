24a1e87eb86bca041065273ad9e37ba744d7455b
PHGMorgan <morgan.hutchens777@gmail.com>
2024-10-15 00:26:53 -0700

    Added player movement

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,6 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        ship.update(dt)
+        ship.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

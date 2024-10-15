47d029cbba73a8da976094803e5898b1c685365c
PHGMorgan <morgan.hutchens777@gmail.com>
2024-10-14 23:56:11 -0700

    Added circleshape and player files, modified main and constants

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 from constants import *
 
 
-
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -13,12 +12,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill((0,0,0))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

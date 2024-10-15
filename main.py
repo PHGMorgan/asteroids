7314e5b2560e020617742a7f5d82130ea0277b90
PHGMorgan <morgan.hutchens777@gmail.com>
2024-10-15 00:38:51 -0700

    Added groups

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from player import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        ship.update(dt)
-        ship.draw(screen)
+        for update in updateable:
+            update.update(dt)
+        for display in drawable:
+            display.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 

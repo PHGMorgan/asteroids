cd337aeebc51535e53471310c2a0a8c43eb7d13b
PHGMorgan <morgan.hutchens777@gmail.com>
2024-10-16 01:49:34 -0700

    Added asteroid, asteroidfield, and shot files. Updated circleshape, constants, main, and player files.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 
 def main():
@@ -9,15 +12,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
+    updateable, drawable, asteroids, shots  = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    Shot.containers = (updateable, drawable, shots)
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -25,6 +32,10 @@ def main():
         screen.fill((0,0,0))
         for update in updateable:
             update.update(dt)
+        for asteroid in asteroids:
+            if ship.collision(asteroid) == True:
+                print("Game over!")
+                return
         for display in drawable:
             display.draw(screen)
         pygame.display.flip()
